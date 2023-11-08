33b04e2b1460da2af133cc876c7256eda24a476a
lucasvil4r <lucasvilar-celestino@hotmail.com>
2023-11-07 22:52:00 -0300

    feature: resolução exercicios angular

--- CursoAngularJS/Properties/AssemblyInfo.cs
+++ CursoAngularJS/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CursoAngularJS")]
+[assembly: AssemblyTitle("AngularJS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CursoAngularJS")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularJS")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2dd25331-1fcf-46f4-8705-5a9e5bec88da")]
+[assembly: Guid("d709cce0-3fbc-4c48-bc2b-c7033d11c0ca")]
 
 // Version information for an assembly consists of the following four values:
 //
